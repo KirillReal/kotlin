1f06160d6e93578e0550d21141e0d58c5bd5b8bc
KirillReal <kiril5471710@gmail.com>
2023-08-06 00:53:09 +0300

    добавил файл Calc и написал в нём методы вычитания, сложения ,деления и умножения

---
+++ src/main/kotlin/ru/job4j/base/Calc.kt
@@ -0,0 +1,31 @@
+package ru.job4j.base
+
+fun add(first : Int, second : Int) : Int {
+    return first + second
+}
+
+fun subtract(first : Int, second : Int) : Int {
+    return first - second
+}
+
+fun multiply(first: Int, second: Int) : Int {
+    return first * second
+}
+
+fun division(first: Double, second: Double) : Double {
+    return first / second
+}
+
+fun main() {
+    val plus = add(1, 1)
+    println("1 + 1 = $plus")
+
+    val minus = subtract(1, 1)
+    println("1 - 1 = $minus")
+
+    val multiply = multiply(2, 5)
+    println("2 * 5 = $multiply")
+
+    val divide = division(15.0, 2.0)
+    println("15.0 / 2.0 = $divide")
+}
